4b0b184bc13fecd486203fe3d57957e639994f7f
Sapranov Andrey <andreika9198@gmail.com>
2026-04-23 13:37:35 +0300

    Создал тес test_03_01_picle_data_to_file_rb

--- main/test_class/pytest_class.py
+++ main/test_class/pytest_class.py
@@ -9,4 +9,14 @@ def test_02_01_picle_data_to_file_wb(pickler_obj):
         'test_key_2': 'test_value_2',
         'test_key_3': 'test_value_3'
     }
-    assert pickler_obj.picle_data_to_file_wb(filename,my_dict) == True
+    assert pickler_obj.picle_data_to_file_wb(filename ,my_dict) == True
+
+def test_03_01_picle_data_to_file_rb(pickler_obj):
+    filename = r'test_save\save_1.txt'
+    assert pickler_obj.picle_data_to_file_rb(filename) == {'test_key_1': 'test_value_1',
+ 'test_key_2': 'test_value_2',
+ 'test_key_3': 'test_value_3'}
+    filename_2 = r'test_save\save_6.txt'
+    assert pickler_obj.picle_data_to_file_rb(filename_2) is None
+    filename_3 = r'test_save\save_2.txt'
+    assert pickler_obj.picle_data_to_file_rb(filename_3) is None
