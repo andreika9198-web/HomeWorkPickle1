904a14d813b396011590cb0ebaa55dc8b7f9b631
Sapranov Andrey <andreika9198@gmail.com>
2026-04-23 13:45:34 +0300

    Создал тест test_05_01_has_feli

--- main/test_class/pytest_class.py
+++ main/test_class/pytest_class.py
@@ -1,8 +1,14 @@
 
 def test_01_01_picle_data_wb(pickler_obj):
+    """
+    Тестируем сохранения данных в байт
+    """
     assert pickler_obj.picle_data_wb('10') == b'\x80\x05\x95\x06\x00\x00\x00\x00\x00\x00\x00\x8c\x0210\x94.'
 
 def test_02_01_picle_data_to_file_wb(pickler_obj):
+    """
+    Тестируем сохранения данных в байт, и записываем в файл
+    """
     filename = r'test_save\save_1.txt'
     my_dict = {
         'test_key_1': 'test_value_1',
@@ -12,11 +18,30 @@ def test_02_01_picle_data_to_file_wb(pickler_obj):
     assert pickler_obj.picle_data_to_file_wb(filename ,my_dict) == True
 
 def test_03_01_picle_data_to_file_rb(pickler_obj):
+    """
+    Тестируем загрузку данных из файла
+    """
     filename = r'test_save\save_1.txt'
     assert pickler_obj.picle_data_to_file_rb(filename) == {'test_key_1': 'test_value_1',
- 'test_key_2': 'test_value_2',
- 'test_key_3': 'test_value_3'}
+    'test_key_2': 'test_value_2',
+    'test_key_3': 'test_value_3'}
     filename_2 = r'test_save\save_6.txt'
     assert pickler_obj.picle_data_to_file_rb(filename_2) is None
     filename_3 = r'test_save\save_2.txt'
     assert pickler_obj.picle_data_to_file_rb(filename_3) is None
+
+def test_04_01_picle_picle_data_rb(pickler_obj):
+    """
+    Тестируем загрузку данных из байта
+    """
+    test_b = b'\x80\x05\x95\x06\x00\x00\x00\x00\x00\x00\x00\x8c\x0210\x94.'
+    assert pickler_obj.picle_data_rb(test_b) == '10'
+
+def test_05_01_has_feli(pickler_obj):
+    """
+    Тестируем на наличие пустого файла
+    """
+    filename = r'test_save\save_1.txt'
+    assert pickler_obj.has_feli(filename) == False
+    filename_1 = r'test_save\save_3.txt'
+    assert pickler_obj.has_feli(filename_1) == True
